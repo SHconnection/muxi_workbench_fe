01ed0bdaa2feaf5ad659cb03c694b50708f241d2
Cruyun <1045671796@qq.com>
2018-12-02 19:39:58 +0800

    feat: fix landing url

--- server/index.js
+++ server/index.js
@@ -18,10 +18,10 @@ app.use(async (ctx, next) => {
     await next();
   } else {
     // for develop
-    ctx.redirect("https://pass.muxixyz.com/?landing=work.muxixyz.com/landing")
+    ctx.redirect("https://user.muxixyz.com/?landing=work.muxixyz.com/landing")
     // for production
     // ctx.redirect(
-    //   "https://pass.muxixyz.com/?landing=localhost:3000/landing"
+    //   "https://user.muxixyz.com/?landing=localhost:3000/landing"
     // );
   }
 });
